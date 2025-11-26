65b67a4028005a353e684720e4e167e131c47da9
murshadking330 <murshadking330@gmail.com>
2025-11-26 16:55:10 +0500

    Delete app/api/list/route.ts

--- app/api/list/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
-
-const db = new PrismaClient();
-
-export async function GET() {
-  const all = await db.link.findMany({ orderBy: { createdAt: "desc" } });
-  return NextResponse.json(all);
-}
